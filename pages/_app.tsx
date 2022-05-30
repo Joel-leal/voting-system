afced191dd2df17977d1602d3a9c9d671fddec85
Joel Morais <joel.l@Joels-MacBook-Air.local>
2022-05-29 22:13:28 -0300

    adicionando CSSReset

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ThemeProvider, theme, CSSReset } from '@chakra-ui/core';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp;
