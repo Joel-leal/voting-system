385742ab9bae9e43469d59a997a8e7b050ef1cbb
Joel Morais <joel.l@Joels-MacBook-Air.local>
2022-05-31 12:06:45 -0300

    adjust favicon add css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,20 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 import { Heading, Link, Text } from '@chakra-ui/core'
 
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Creating App of Voting</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
+      <main>
         <Heading as="h1" size="2xl" mb="2">
           Welcome to <Link color="teal.500" href="https://nextjs.org">Voting-System!</Link>
         </Heading>
-        <Text className={styles.description}>
+        <Text>
           This is another a project podCodar{' '}
           <Link color="#8e407a" href="https://www.notion.so/podcodar/VS-42132534db42406e9e9fe1e6defa0ab9">Wiki.</Link>
         </Text>
