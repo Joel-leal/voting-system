046dd35722dc011dab49337783c3e081c1796072
Joel Morais <joel.l@Joels-MacBook-Air.local>
2022-05-31 10:59:16 -0300

    correction following suggestions

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { Heading, Link } from '@chakra-ui/core'
+import { Heading, Link, Text } from '@chakra-ui/core'
 
 import type { NextPage } from 'next';
 
@@ -9,15 +9,16 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <Head>
         <title>Creating App of Voting</title>
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-      <Heading as="h1" size="2xl" mb="2">
-        Welcome to <Link color="teal.500" href="https://nextjs.org">Voting-System!</Link>
-      </Heading>
-        <p className={styles.description}>
+        <Heading as="h1" size="2xl" mb="2">
+          Welcome to <Link color="teal.500" href="https://nextjs.org">Voting-System!</Link>
+        </Heading>
+        <Text className={styles.description}>
           This is another a project podCodar{' '}
           <Link color="#8e407a" href="https://www.notion.so/podcodar/VS-42132534db42406e9e9fe1e6defa0ab9">Wiki.</Link>
-        </p>
+        </Text>
       </main>
     </div>
   );
