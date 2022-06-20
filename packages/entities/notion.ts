fee5822858515ce5ac185f4605496c7fb7593360
frattezi <pfrattezi@gmail.com>
2022-06-20 15:54:21 -0300

    feat: move interface declarations to entities

--- packages/entities/notion.ts
+++ packages/entities/notion.ts
@@ -1,4 +1,16 @@
-export type CreateElectionResultResponse = {
+export interface CreateElectionResultResponse {
   message: string;
   page_id?: string;
-};
+}
+interface Party {
+  name: string;
+  members: string[];
+}
+
+export interface CreateResultPage {
+  database_id: string;
+  election_name: string;
+  election_id: string;
+  winner_party: Party;
+  looser_party: Party;
+}
