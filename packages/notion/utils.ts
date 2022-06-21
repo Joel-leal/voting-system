7a2983013f7efeb6d1e7b68e0a70f8c37636ecc3
frattezi <pfrattezi@gmail.com>
2022-06-21 20:58:51 -0300

    feat: enhance notion sdk types

--- packages/notion/utils.ts
+++ packages/notion/utils.ts
@@ -1,11 +1,21 @@
-import { Title, LooseObject, Result } from '@packages/notion/sdk';
+import {
+  Title,
+  LooseObject,
+  Result,
+  RichText,
+  Select,
+} from '@packages/notion/sdk';
 
 export function extractPagesFromQuery(pages: Result[]) {
   const extractedPages = pages.map((page) => {
     const pageProperties = _extractProperties(page);
     return {
-      electionId: page.id,
-      electionName: pageProperties.Name,
+      electionId: page?.id,
+      electionName: pageProperties?.Name,
+      partySlug: pageProperties?.Sigla,
+      candidateName: pageProperties?.Candidato,
+      viceCandidateName: pageProperties?.Vice,
+      code: pageProperties?.Codigo,
     };
   });
 
@@ -21,13 +31,31 @@ function _extractProperties(result: Result): LooseObject {
       switch (value.type) {
         case 'title':
           parsedResults[key] = _titleHandler(value);
+          break;
+        case 'select':
+          parsedResults[key] = _selectHandler(value);
+          break;
+        case 'rich_text':
+          parsedResults[key] = _richTextHandler(value);
       }
     });
   }
 
+  console.log(parsedResults);
   return parsedResults;
 }
 
 function _titleHandler(propertyValue: Title): string {
   return propertyValue.title[0].plain_text;
+}
+
+function _richTextHandler(propertyValue: RichText): string {
+  return propertyValue.rich_text[0].plain_text;
+}
+
+function _selectHandler(propertyValue: Select): string {
+  if (propertyValue?.select) {
+    return propertyValue.select.name;
+  }
+  return '';
 }
