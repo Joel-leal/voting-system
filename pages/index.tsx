023dacee845fbf4ff50f8f9c1ea6b93291b2152a
joel-leal <joel.lasi@hotmail.com>
2022-06-20 21:27:57 -0300

    criando a home-page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,21 +5,23 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <Flex h='100vh' w='100vw' alignItems='center' justifyContent='center' >
-      <Box >
+    <Flex h="100vh" w="100vw" alignItems="center" justifyContent="center">
+      <Box>
         <Head>
           <title>Eleições 2018</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <main>
           <Heading as="h1" size="2xl" mb="2">
-            Selecione a turma de votação{' '}
-            <Select>
-            <option value='option1'>Sexto-ano</option>
-            <option value='option2'>Sétimo-ano</option>
-            <option value='option3'>Oitavo-ano</option>
-            <option value='option3'>Nono-ano</option>
-          </Select>
+            Selecione uma votação{' '}
+            <Box>
+              <Select>
+                <option value="sexto-ano">Sexto-ano</option>
+                <option value="setimo-ano">Sétimo-ano</option>
+                <option value="oitavo-ano">Oitavo-ano</option>
+                <option value="nono-ano">Nono-ano</option>
+              </Select>
+            </Box>
           </Heading>
         </main>
       </Box>
