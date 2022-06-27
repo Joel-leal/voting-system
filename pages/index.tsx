6a42362b6e4b75022106c9c27c0e7c5fc58e2dee
joel-leal <joel.lasi@hotmail.com>
2022-06-27 15:13:51 -0300

    fix: adjusting select and inserting button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Heading, Box, Flex, Select } from '@chakra-ui/react';
+import { Heading, Box, Flex, Select, Button } from '@chakra-ui/react';
 import Head from 'next/head';
 
 import type { NextPage } from 'next';
@@ -8,21 +8,30 @@ const Home: NextPage = () => {
     <Flex h="100vh" w="100vw" alignItems="center" justifyContent="center">
       <Box>
         <Head>
-          <title>Eleições 2018</title>
+          <title>Eleições 2022</title>
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <main>
-          <Heading as="h1" size="2xl" mb="2">
+          <Heading as="h1" size="2xl">
             Selecione uma votação{' '}
-            <Box>
-              <Select>
-                <option value="sexto-ano">Sexto-ano</option>
-                <option value="setimo-ano">Sétimo-ano</option>
-                <option value="oitavo-ano">Oitavo-ano</option>
-                <option value="nono-ano">Nono-ano</option>
-              </Select>
-            </Box>
           </Heading>
+          <Box
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            width="100%"
+            py="10px"
+          >
+            <Select>
+              <option value="sexto-ano">Sexto-ano</option>
+              <option value="setimo-ano">Sétimo-ano</option>
+              <option value="oitavo-ano">Oitavo-ano</option>
+              <option value="nono-ano">Nono-ano</option>
+            </Select>
+            <Button colorScheme="blue" marginLeft="10px">
+              Buscar
+            </Button>
+          </Box>
         </main>
       </Box>
     </Flex>
