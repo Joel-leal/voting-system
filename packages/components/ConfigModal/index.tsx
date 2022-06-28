fff1b920d3b91cd5d6ab9386a9015ec9278cca3e
frattezi <pfrattezi@gmail.com>
2022-06-27 21:59:35 -0300

    feat: add reminder to modal

--- packages/components/ConfigModal/index.tsx
+++ packages/components/ConfigModal/index.tsx
@@ -7,6 +7,10 @@ import {
   ModalBody,
   ModalFooter,
   Button,
+  Box,
+  Text,
+  Link,
+  useColorModeValue,
 } from '@chakra-ui/react';
 import { ChangeEvent, useState } from 'react';
 
@@ -14,6 +18,7 @@ import ConfigForm from '@packages/components/ConfigModal/ConfigForm';
 import { IConfigModal, FormState } from '@packages/entities/config-modal';
 
 export default function ConfigModal({ isOpen, onClose }: IConfigModal) {
+  const boxBgColor = useColorModeValue('gray.100', 'gray.900');
   const [formState, setFormState] = useState<FormState>({
     notionApiKey: '',
     electionDatabaseId: '',
@@ -40,6 +45,19 @@ export default function ConfigModal({ isOpen, onClose }: IConfigModal) {
 
         <ModalBody>
           <ConfigForm onChange={onChangeInput} formState={formState} />
+
+          <Box bgColor={boxBgColor} padding="10px" marginTop="10px">
+            <Text fontSize="0.8em">
+              {' '}
+              Caso ainda não tenha estes dados lembre-se de seguir nosso{' '}
+              <Link
+                color="teal.500"
+                href="https://www.notion.so/podcodar/Docs-7e84b843b0ee496d8f4bf3e59683072a"
+              >
+                tutorial de setup do projeto
+              </Link>
+            </Text>
+          </Box>
         </ModalBody>
 
         <ModalFooter>
