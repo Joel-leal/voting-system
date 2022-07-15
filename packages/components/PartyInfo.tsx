1f814dad6078a4972fd72aeca89689d9b3f9561a
joel-leal <joel.lasi@hotmail.com>
2022-07-15 14:15:09 -0300

    fix: correction marginLeft for padding.

--- packages/components/PartyInfo.tsx
+++ packages/components/PartyInfo.tsx
@@ -16,18 +16,19 @@ export default function PartyInfo({ candidate, vice, partido }: IPartyInfo) {
       h="35vh"
       justifyContent="center"
       alignItems="left"
+      px="20px"
     >
-      <Box display="flex" marginLeft="10%" py="20px">
+      <Box display="flex" py="5px">
         <Text fontWeight="bold" fontSize="20px">
           Candidato: {candidate}
         </Text>
       </Box>
-      <Box display="flex" marginLeft="10%">
+      <Box display="flex" py="5px">
         <Text fontWeight="bold" fontSize="20px">
           Vice: {vice}
         </Text>
       </Box>
-      <Box display="flex" marginLeft="10%" py="20px">
+      <Box display="flex" py="5px">
         <Text fontWeight="bold" fontSize="20px">
           Partido: {partido}
         </Text>
