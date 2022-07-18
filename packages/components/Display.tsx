4be3fd07e11e9a664b034ddd77f8786c438ba41b
Guilherme barbosa <guilhermebarbo17@hotmail.com>
2022-07-18 16:31:29 -0300

    implement candidateCard in display

--- packages/components/Display.tsx
+++ packages/components/Display.tsx
@@ -1,4 +1,5 @@
-import { Flex, Grid } from '@chakra-ui/react';
+import { Box, Flex, Grid } from '@chakra-ui/react';
+import CandidateCard from './CadidateCard';
 
 import PartyInfo from './PartyInfo';
 
@@ -8,8 +9,27 @@ export default function Display() {
       <Flex bg="#FED7D7" h="100%" justifyContent="center" alignItems="end">
         <PartyInfo />
       </Flex>
-      <Flex bg="#FEFCBF" h="100%" justifyContent="center" alignItems="center">
-        Contender box
+      <Flex
+        bg="#FEFCBF"
+        h="100%"
+        justifyContent="center"
+        alignItems="flex-end"
+        flexDirection="column"
+      >
+        <CandidateCard
+          size={'140px'}
+          margin={'1rem'}
+          source={
+            'https://static.wixstatic.com/media/0bd8b5_779385fe12ff4155947b97f75533ffb9~mv2.jpg/v1/fill/w_458,h_458,al_c,lg_1,q_80,enc_auto/BONECO%20PARA%20PERFIL.jpg'
+          }
+        />
+        <CandidateCard
+          size={'120px'}
+          margin={'1.7rem'}
+          source={
+            'https://static.wixstatic.com/media/0bd8b5_779385fe12ff4155947b97f75533ffb9~mv2.jpg/v1/fill/w_458,h_458,al_c,lg_1,q_80,enc_auto/BONECO%20PARA%20PERFIL.jpg'
+          }
+        />
       </Flex>
     </Grid>
   );
