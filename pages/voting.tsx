630c51b76eb0d7e973a2f7fdab6cbba916e99745
joel-leal <joel.lasi@hotmail.com>
2022-07-25 17:45:27 -0300

    feat: add instruction text in page of the voting

--- pages/voting.tsx
+++ pages/voting.tsx
@@ -1,6 +1,7 @@
 import { Box, Grid } from '@chakra-ui/react';
 
 import Display from '@packages/components/Display';
+import VotingInstructions from '@packages/components/VotingInstructions';
 
 import type { NextPage } from 'next';
 
@@ -8,7 +9,10 @@ const VotingPage: NextPage = () => {
   return (
     <Grid templateColumns="2fr 1fr" gap="3" h="100vh" p="10">
       <Display />
-      <Box bg="blue" h="100%" boxShadow="dark-lg" />
+      <Box bg="blue" h="100%" boxShadow="dark-lg">
+        {' '}
+        <VotingInstructions />
+      </Box>
     </Grid>
   );
 };
